b9639c2d1de779446a8d4f08d31ff40464f69f71
Anton Shchukin <a.a.shchukin@gmail.com>
2023-01-13 23:10:49 +0700

    Make header responsive

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,10 +2,9 @@
 	import { base } from '$app/paths';
 </script>
 
-<div class="flex flex-row inline-flex items-center justify-center w-full">
-
-	<hr class="w-64 h-px my-8 border-0 dark:bg-white">
-	<a href="/" class="flex-initial"><img src="{base}/Logo_Vector_Red.png" class="m-auto w-32" alt="The Logo" /></a>
-	<hr class="w-64 h-px my-8 border-0 dark:bg-white">
+<div class="flex flex-row inline-flex items-center justify-center w-full px-10">
+    <hr class="grow h-px my-8 dark:bg-white">
+	<a href="/" class="flex-initial"><img src="{base}/Logo_Vector_Red.png" class="m-auto w-24" alt="The Logo" /></a>
+    <hr class="grow h-px my-8 dark:bg-white">
 
 </div>
