439480a4b0b65d80f528a773b2a11f5c827982d4
Anton Shchukin <a.a.shchukin@gmail.com>
2023-01-13 22:23:52 +0700

    Navbar wrapping

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,5 +1,5 @@
 <nav class="flex justify-center p-4">
-	<div class="flex text-center align-end gap-4">
+	<div class="flex flex-wrap justify-center text-center align-end gap-4">
 		<a href="/events" class="navlink flex items-center">our events</a>
 		<a href="/event-calendar" class="navlink flex items-center">all events</a>
 		<a href="/rope-jams" class="navlink flex items-center">rope jams</a>
