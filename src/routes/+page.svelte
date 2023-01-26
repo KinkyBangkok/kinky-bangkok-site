329067706546a8a9f239ce3f8f0ea39e81e9191b
Robert Ernst <9050259+Robert-Ernst@users.noreply.github.com>
2023-01-26 13:23:50 +0700

    Removed two unnecessary div container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,30 @@
-<svelte:head>
-	<title>Kinky Bangkok - join the community!</title>
-	<meta name="description" content="We are a community based in Bangkok focused on Kink and BDSM. We are open to locals, expats, visitors and new faces from all around the world. We have regular events and parties in Bangkok." />
-</svelte:head>
-
 <script>
 	import { base } from '$app/paths';
 	import Navbar from './Navbar.svelte';
 </script>
 
+<svelte:head>
+	<title>Kinky Bangkok - join the community!</title>
+	<meta
+		name="description"
+		content="We are a community based in Bangkok focused on Kink and BDSM. We are open to locals, expats, visitors and new faces from all around the world. We have regular events and parties in Bangkok."
+	/>
+</svelte:head>
 
 <main class="h-full flex justify-center text-center flex-col">
 	<div class="text-center w-full mb-4">
-		<img src="{base}/Logo_Vector_Red.png" class="m-auto sm:max-w-[260px] max-w-[50%]" alt="The Logo" />
+		<img
+			src="{base}/Logo_Vector_Red.png"
+			class="m-auto sm:max-w-[260px] max-w-[50%]"
+			alt="The Logo"
+		/>
 	</div>
 
-	<article class="text-white max-w-2xl mx-auto px-12 my-4">
-		<h1><b>Kinky Bangkok</b> - KBK for short - is a community based in Bangkok focused on Kink, BDSM and
+	<h1 class="text-white max-w-2xl mx-auto px-12 my-4">
+		<b>Kinky Bangkok</b> - KBK for short - is a community based in Bangkok focused on Kink, BDSM and
 		Sex. We are open to locals, expats, visitors and new faces from all around the world. We have regular
-		play parties, bdsm workshops and fetish events in Bangkok.</h1>
-	</article>
+		play parties, bdsm workshops and fetish events in Bangkok.
+	</h1>
 
 	<Navbar />
 </main>
