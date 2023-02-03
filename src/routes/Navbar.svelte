308f3bf2927f8b847c7f81f0289270bb6b5a0c44
Robert Ernst <9050259+Robert-Ernst@users.noreply.github.com>
2023-02-03 12:12:53 +0700

    Remove unused KBK events from navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,7 +1,5 @@
 <nav class="flex justify-center p-4">
 	<div class="flex flex-wrap justify-center text-center align-end gap-4">
-		<a href="/events" class="navlink">event overview</a>
-		<a href="/rope-jams" class="navlink">rope jams</a>
 		<a href="/communities" class="navlink">community list</a>
 		<a href="/event-calendar" class="navlink">community events</a>
 	</div>
