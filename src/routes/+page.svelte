1f51e8a23cf4ab621e2b257dab26522ebd046a40
Robert Ernst <9050259+Robert-Ernst@users.noreply.github.com>
2023-02-03 12:46:32 +0700

    Update SEO on index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <svelte:head>
-	<title>Kinky Bangkok - modern BDSM community, event and workshop organizer in Thailand</title>
+	<title>Kinky Bangkok - modern BDSM community in Thailand</title>
 	<meta
 		name="description"
-		content="We are a community based in Bangkok focused on Kink and BDSM. We are open to locals, expats, visitors and new faces from all around the world. We have regular events and parties in Bangkok."
+		content="We are a community based in Bangkok focused on Kink and BDSM. We are open to locals, expats, visitors and new faces from all around the world"
 	/>
 </svelte:head>
 
